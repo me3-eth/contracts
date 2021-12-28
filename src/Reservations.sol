8835f8346954f59add853658358f260febf8cea7
charchar <87212793+0xcharchar@users.noreply.github.com>
2021-12-28 00:52:17 +0000

    more unit tests

--- src/Reservations.sol
+++ src/Reservations.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts/access/Ownable.sol";
 
+library Errors {
+  string internal constant ReservationTaken = "Reservation already taken";
+  string internal constant ReservationOwnerOnly = "Must be an owner to modify reservation";
+}
+
+/// @title Reservations
+/// @notice Reserve a number of subdomains for later use and be able to transfer them to another owner
 contract Reservations is Ownable {
-  // All reservations
   mapping(bytes32 => address) public s_reservations;
 
   constructor(address contractOwner) {
@@ -31,8 +37,25 @@ contract Reservations is Ownable {
     transferOwnership(contractOwner);
   }
 
+  /// @notice Reserve a subdomain to a particular address
+  /// @param subdomain The subdomain to reserve
+  /// @param reserveOwner The address that will own the reservation
   function add (bytes32 subdomain, address reserveOwner) external onlyOwner {
-    require(s_reservations[subdomain] == address(0), "Reservation already taken");
+    require(s_reservations[subdomain] == address(0), Errors.ReservationTaken);
     s_reservations[subdomain] = reserveOwner;
+  }
+
+  /// @notice Change the owner of a subdomain reservation. Only possible if you are the reservation owner or the contract owner
+  /// @param subdomain The subdomain to update
+  /// @param newOwner Who to change the owner to. Use address(0) to free the reservation
+  function modify (bytes32 subdomain, address newOwner) external {
+    bool hasAccess = (
+      (s_reservations[subdomain] == address(msg.sender)) ||
+      (owner() == address(msg.sender))
+    );
+
+    require(hasAccess, Errors.ReservationOwnerOnly);
+
+    s_reservations[subdomain] = newOwner;
   }
 }
--- src/test/Reservations.t.sol
+++ src/test/Reservations.t.sol
@@ -4,36 +4,68 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "../Reservations.sol";
 
-contract ReservationsTest {
+contract User {
+  Reservations internal r;
+
+  function init (address _r) public {
+    r = Reservations(_r);
+  }
+
+  function addReservation (bytes32 subdomain, address reserveOwner) public {
+    r.add(subdomain, reserveOwner);
+  }
+
+  function modifyReservation (bytes32 subdomain, address newOwner) public {
+    r.modify(subdomain, newOwner);
+  }
+}
+
+contract ReservationsTest is DSTest {
   Reservations r;
 
-  address constant newOwner = 0x95a647B3d8a3F11176BAdB799b9499C671fa243a;
+  User internal reservationCreator;
+
   address constant signer = 0x71950741B6CC9414422230EC2Daf097a1936A06b;
 
   function setUp() public {
-    r = new Reservations(newOwner);
+    reservationCreator = new User();
+    r = new Reservations(address(reservationCreator));
+    reservationCreator.init(address(r));
   }
 
   function testReservationsCreatedInConstructor() public {
-    require(r.s_reservations("admin") == 0x95a647B3d8a3F11176BAdB799b9499C671fa243a);
-    require(r.s_reservations("www") == 0x95a647B3d8a3F11176BAdB799b9499C671fa243a);
+    require(r.s_reservations("admin") == address(reservationCreator));
+    require(r.s_reservations("www") == address(reservationCreator));
   }
 
   function testOwnershipTransferredFromSenderToChosen() public {
-    // Using the address from setUp as new owner
-    require(r.owner() == 0x95a647B3d8a3F11176BAdB799b9499C671fa243a);
+    require(r.owner() == address(reservationCreator));
   }
 
   function testAddNewReservation() public {
-    r.add("youknow", signer);
+    reservationCreator.addReservation("youknow", signer);
     require(r.s_reservations("youknow") == signer, "Reservation not completed");
   }
 
-  function testFailWhenReservationTaken() public {
-    try r.add("admin", signer) {
-      revert();
+  function testUnableToAddNewReservationWhenExisting() public {
+    try reservationCreator.addReservation("admin", signer) {
+      fail();
     } catch Error(string memory error) {
-      require(keccak256(abi.encode(error)) == keccak256("Reservation already taken"), "Unexpected error occurring");
+      assertEq(error, Errors.ReservationTaken, "Reservation should be taken");
     }
+  }
+
+  function testUnableToModifyReservationWhenNotOwner() public {
+    try r.modify("admin", address(reservationCreator)) {
+      fail();
+    } catch Error(string memory error) {
+      assertEq(error, Errors.ReservationOwnerOnly);
+    }
+  }
+
+  function testReservationOwnerChanged(address newOwner) public {
+    reservationCreator.modifyReservation("admin", newOwner);
+
+    assertEq(r.s_reservations("admin"), newOwner);
   }
 }
